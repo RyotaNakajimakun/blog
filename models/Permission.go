1f213572a611bbce7ebc7976939e65f81dc9c72a
RyotaNakajimakun <hope.for.world.peace0208@gmail.com>
2019-05-03 01:16:40 +0900

    [change] RoleとPermissionを分別

---
+++ models/Permission.go
@@ -0,0 +1,48 @@
+package models
+
+type Permission struct {
+	ID          int    `form:"id" gorm:"primary_key"`
+	Name        string `form:"name" gorm:"unique; not null"`
+	DisplayName string `form:"display_name" gorm:"not null"`
+	Detail      string `form:"detail" gorm:"default:null"`
+}
+
+func NewPermission(name string, displayName string, detail string) Permission {
+	permission := Permission{
+		Name:        name,
+		DisplayName: displayName,
+		Detail:      detail,
+	}
+	return permission
+}
+
+func HasPermission(name string) interface{} {
+	type PermissionCheck struct {
+		Permission
+		Check bool
+	}
+	var HasPermissions []PermissionCheck
+	var role Role
+	var permissions []Permission
+
+	db := GetDB()
+	db.Where("name = ?", name).First(&role)
+	db.Model(&role).Related(&role.HasPermission, "HasPermission")
+	db.Find(&permissions)
+
+	has := make([]bool, len(permissions))
+	for i := range role.HasPermission {
+		var permissionID int = role.HasPermission[i].ID - 1
+		has[permissionID] = true
+	}
+
+	for i := range permissions {
+		HasPermissions = append(HasPermissions, PermissionCheck{})
+		HasPermissions[i].ID = permissions[i].ID
+		HasPermissions[i].Name = permissions[i].Name
+		HasPermissions[i].DisplayName = permissions[i].DisplayName
+		HasPermissions[i].Detail = permissions[i].Detail
+		HasPermissions[i].Check = has[i]
+	}
+	return HasPermissions
+}
--- models/role.go
+++ models/role.go
@@ -8,13 +8,6 @@ type Role struct {
 	HasPermission []Permission `gorm:"many2many:role_permission;"`
 }
 
-type Permission struct {
-	ID          int    `form:"id" gorm:"primary_key"`
-	Name        string `form:"name" gorm:"unique; not null"`
-	DisplayName string `form:"display_name" gorm:"not null"`
-	Detail      string `form:"detail" gorm:"default:null"`
-}
-
 func NewRole(name string, displayName string, detail string) Role {
 	role := Role{
 		Name:        name,
@@ -22,44 +15,4 @@ func NewRole(name string, displayName string, detail string) Role {
 		Detail:      detail,
 	}
 	return role
-}
-
-func NewPermission(name string, displayName string, detail string) Permission {
-	permission := Permission{
-		Name:        name,
-		DisplayName: displayName,
-		Detail:      detail,
-	}
-	return permission
-}
-
-func HasPermission(name string) interface{} {
-	type PermissionCheck struct {
-		Permission
-		Check bool
-	}
-	var HasPermissions []PermissionCheck
-	var role Role
-	var permissions []Permission
-
-	db := GetDB()
-	db.Where("name = ?", name).First(&role)
-	db.Model(&role).Related(&role.HasPermission, "HasPermission")
-	db.Find(&permissions)
-
-	has := make([]bool, len(permissions))
-	for i := range role.HasPermission {
-		var permissionID int = role.HasPermission[i].ID - 1
-		has[permissionID] = true
-	}
-
-	for i := range permissions {
-		HasPermissions = append(HasPermissions, PermissionCheck{})
-		HasPermissions[i].ID = permissions[i].ID
-		HasPermissions[i].Name = permissions[i].Name
-		HasPermissions[i].DisplayName = permissions[i].DisplayName
-		HasPermissions[i].Detail = permissions[i].Detail
-		HasPermissions[i].Check = has[i]
-	}
-	return HasPermissions
 }
