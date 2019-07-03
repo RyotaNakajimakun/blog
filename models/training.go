cd3844e11c3cfc54ac07c7bc0369c2468df7d786
nakajima_ryota <nakajima_ryota@3-ize.jp>
2019-07-03 18:01:06 +0900

    [add] ShouldBind用の構造体を実装 [make OPTIMIZE models/training.go]

--- models/training.go
+++ models/training.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"github.com/k0kubun/pp"
+	"time"
+)
 
 type Training struct {
 	Model
@@ -10,4 +13,24 @@ type Training struct {
 	StartingTime time.Time  `form:"starting_time"`
 	EndingTime   time.Time  `form:"ending_time"`
 	Exercise     []Exercise `gorm:`
+}
+
+type TrainingForm struct {
+	Note         string     `form:"note" gorm:"not null;default:''"`
+	TrainingDay  string     `form:"training_day"`
+	StartingTime string     `form:"starting_time"`
+	EndingTime   string     `form:"ending_time"`
+	Exercise     []Exercise `gorm:`
+}
+
+//_@OPTIMIZE レシーバーを利用して実装した方が無駄が少ない
+func SetTrainingData(tf TrainingForm, exercises []Exercise) Training {
+	var trainingData Training
+	trainingData.Note = tf.Note
+	trainingData.TrainingDay, _ = time.Parse("2006-01-02", tf.TrainingDay)
+	trainingData.StartingTime, _ = time.Parse("15:04", tf.StartingTime)
+	trainingData.EndingTime, _ = time.Parse("15:04", tf.EndingTime)
+
+	trainingData.Exercise = exercises
+	return trainingData
 }
