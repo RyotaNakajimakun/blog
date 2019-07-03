fc9d498f81ad75a1df368bdfafa55d8e1c89ddcb
nakajima_ryota <nakajima_ryota@3-ize.jp>
2019-07-03 18:09:17 +0900

    [add] トレーニングをDBに保存できるように実装 [make TODO controllers/training.go] ShouldBindを利用して実装するようにした方が良かった

--- controllers/training.go
+++ controllers/training.go
@@ -2,9 +2,11 @@ package controllers
 
 import (
 	"github.com/RyotaNakajimakun/blog/models"
+	"github.com/Sirupsen/logrus"
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
-	"github.com/k0kubun/pp"
 	"net/http"
+	"strconv"
 )
 
 func TrainingIndex(c *gin.Context) {
@@ -15,18 +17,42 @@ func TrainingIndex(c *gin.Context) {
 }
 
 func TrainingPost(c *gin.Context) {
-	 h := DefaultH(c)
-	 h["Title"] = "TrainingPost"
+	h := DefaultH(c)
+	h["Title"] = "TrainingPost"
 
-	 c.HTML(http.StatusOK, "training/post", h)
+	c.HTML(http.StatusOK, "training/post", h)
 }
 
+//@TODO Validationの処理を実装する必要がある
 func TrainingSave(c *gin.Context) {
-	training := models.Training{}
-	c.ShouldBind(training)
+	var err error
+	var trainingData models.TrainingForm
+	formValues := c.Request.Form
 
-	pp.Println(training)
-	pp.Println(c.Request.Form)
+	exercises := make([]models.Exercise, len(formValues["training_name"]))
+	for cnt, exercise := range exercises {
+		exercise.TrainingName = formValues["training_name"][cnt]
+		exercise.Rep, err = strconv.Atoi(formValues["rep"][cnt])
+		exercise.Set, err = strconv.Atoi(formValues["set"][cnt])
+		exercise.Interval, err = strconv.Atoi(formValues["interval"][cnt])
+		exercise.WeightID, err = strconv.Atoi(formValues["uint"][cnt])
+		exercises[cnt] = exercise
+	}
 
-	c.Redirect(http.StatusMovedPermanently, "http://localhost:8080/admin/training")
+	db := models.GetDB()
+	if err = c.ShouldBind(&trainingData); err != nil {
+		session := sessions.Default(c)
+		session.AddFlash(err.Error())
+		session.Save()
+		c.Redirect(http.StatusSeeOther, "/admin/training")
+		return
+	}
+	Data := models.SetTrainingData(trainingData, exercises)
+	if err = db.Save(&Data).Error; err != nil {
+		c.HTML(http.StatusInternalServerError, "errors/500", nil)
+		logrus.Error(err)
+		return
+	}
+
+	c.Redirect(http.StatusMovedPermanently, "/admin/training")
 }
