a102fea716f4488573a7292bb1ea824746407af0
nakajima_ryota <nakajima_ryota@3-ize.jp>
2019-07-03 17:53:02 +0900

    [change] exerciseのintを型統一

--- models/exercise.go
+++ models/exercise.go
@@ -1,14 +1,11 @@
 package models
 
-import "time"
-
 type Exercise struct {
-	TrainingID   uint64    `gorm:"not null"`
-	TrainingName string    `form:"training_name" gorm:"not null;default:''"`
-	WeightID     uint8     `form:"weight_id"     gorm:"not null;default:0"`
-	Rep          uint16    `form:"rep"           gorm:"not null;default:0"`
-	Set          uint8     `form:"set"           gorm:"not null;default:0"`
-	Interval     uint16    `form:"interval"      gorm:"not null;default:0"`
-	Time         time.Time `form:"time" gorm:"not null;"`
+	TrainingID   int    `gorm:"not null"`
+	TrainingName string `form:"training_name" gorm:"not null;default:''"`
+	WeightID     int    `form:"weight_id"     gorm:"not null;default:0"`
+	Rep          int    `form:"rep"           gorm:"not null;default:0"`
+	Set          int    `form:"set"           gorm:"not null;default:0"`
+	Interval     int    `form:"interval"      gorm:"not null;default:0"`
 	Weight       Uint //※Uintはモデル名
 }
